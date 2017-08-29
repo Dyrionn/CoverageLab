0ec6abb7f536487550cfe93439541b883e492159
Artur Pianor <artur.pianor@gmail.com>
2017-08-29 19:41:54 -0300

    Refatorando configuração do nuget 2

--- CoverageLab/Lib/DummyFunctions.cs
+++ CoverageLab/Lib/DummyFunctions.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CoverageLab.Lib
 {
@@ -11,7 +7,7 @@ namespace CoverageLab.Lib
 
         public bool DummyFunctionOne(string id)
         {
-            return !String.IsNullOrEmpty(id); ;
+            return !String.IsNullOrEmpty(id);
         }
 
         public bool DummyFunctionTwo()
